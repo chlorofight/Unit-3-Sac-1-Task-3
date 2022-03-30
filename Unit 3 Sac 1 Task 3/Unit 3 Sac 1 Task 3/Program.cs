8c2aca28a19cdfa8547f0753abe3b59ab9e40b6f
chlorofight <76894476+chlorofight@users.noreply.github.com>
2022-03-30 13:44:27 +1100

    Add files via upload

---
+++ Unit 3 Sac 1 Task 3/Unit 3 Sac 1 Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_3_Sac_1_Task_3
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Unit 3 Sac 1 Task 3/Unit 3 Sac 1 Task 3/Sale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_3_Sac_1_Task_3
+{
+    internal class Sale
+    {
+        public string Textbook { get; set; }
+        public string Subject { get; set; }
+        public string Seller { get; set; }
+        public string Purchaser { get; set; }
+        public float PurchasePrice { get; set; }
+        public string SalePrice { get; set;}
+        //string incase there is no rating
+        public string Rating { get; set; }
+    }
+}
