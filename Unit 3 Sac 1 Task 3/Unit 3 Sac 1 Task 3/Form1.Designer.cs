2f11c9f0db23d0f1079f709d5af2dcb42e882770
chlorofight <76894476+chlorofight@users.noreply.github.com>
2022-03-30 13:40:36 +1100

    Add files via upload

---
+++ Unit 3 Sac 1 Task 3/Unit 3 Sac 1 Task 3/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Unit_3_Sac_1_Task_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbFilter = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.cmbAsc = new System.Windows.Forms.ComboBox();
+            this.lblASC = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbFilter
+            // 
+            this.cmbFilter.FormattingEnabled = true;
+            this.cmbFilter.Items.AddRange(new object[] {
+            "Textbook",
+            "Subject",
+            "Rating"});
+            this.cmbFilter.Location = new System.Drawing.Point(228, 46);
+            this.cmbFilter.Name = "cmbFilter";
+            this.cmbFilter.Size = new System.Drawing.Size(121, 24);
+            this.cmbFilter.TabIndex = 0;
+            this.cmbFilter.SelectedIndexChanged += new System.EventHandler(this.cmbFilter_SelectedIndexChanged);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(375, 48);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(196, 22);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1318, 340);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Textbook";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Visible = false;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Subject";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Visible = false;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Seller";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Visible = false;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Purchaser";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Visible = false;
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Purchased Price";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Visible = false;
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Sale Price";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Visible = false;
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Rating";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Visible = false;
+            this.Column7.Width = 125;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(35, 48);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilter.Location = new System.Drawing.Point(222, 9);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(78, 32);
+            this.lblFilter.TabIndex = 4;
+            this.lblFilter.Text = "Filter";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(380, 9);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(110, 32);
+            this.lblSearch.TabIndex = 5;
+            this.lblSearch.Text = "Search";
+            // 
+            // cmbAsc
+            // 
+            this.cmbAsc.FormattingEnabled = true;
+            this.cmbAsc.Items.AddRange(new object[] {
+            "ASC",
+            "DESC"});
+            this.cmbAsc.Location = new System.Drawing.Point(604, 46);
+            this.cmbAsc.Name = "cmbAsc";
+            this.cmbAsc.Size = new System.Drawing.Size(121, 24);
+            this.cmbAsc.TabIndex = 6;
+            this.cmbAsc.SelectedIndexChanged += new System.EventHandler(this.cmbAsc_SelectedIndexChanged);
+            // 
+            // lblASC
+            // 
+            this.lblASC.AutoSize = true;
+            this.lblASC.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblASC.Location = new System.Drawing.Point(601, 9);
+            this.lblASC.Name = "lblASC";
+            this.lblASC.Size = new System.Drawing.Size(391, 32);
+            this.lblASC.TabIndex = 7;
+            this.lblASC.Text = "Choose ASC or DESC (rating)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1618, 947);
+            this.Controls.Add(this.lblASC);
+            this.Controls.Add(this.cmbAsc);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cmbFilter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbFilter;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.ComboBox cmbAsc;
+        private System.Windows.Forms.Label lblASC;
+    }
+}
+
---
+++ Unit 3 Sac 1 Task 3/Unit 3 Sac 1 Task 3/Form1.cs
@@ -0,0 +1,204 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_3_Sac_1_Task_3
+{
+    public partial class Form1 : Form
+    {
+        //Unit 3 Sac 1 Task 3 , Leonardo Bini, 24/03/2022
+        string filter;
+        bool ASC = true;
+        List<Sale> sales = new List<Sale>();
+        BindingSource bs = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bs.DataSource = sales;
+            dataGridView1.DataSource = bs;
+        }
+
+        //Function: Open a file browser
+        //Input: NA
+        //Output: Function main, message with error, message if no file selected
+        private void openFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            try
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //only run function if file selected
+                    main(ofd.FileName);
+                }
+                //if the file browser is closed
+                else if (ofd.ShowDialog() == DialogResult.Cancel)
+                {
+                    //make sure that no file is selected
+                    MessageBox.Show("No file selected");
+                }
+            }
+            //if the file is open returneror
+            catch(System.IO.IOException)
+            {
+                MessageBox.Show("File Cannot Be Open", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show("Not valid", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Function: read and add the lines in the file to objects
+        //Input: string, temp
+        //Output:string[], List<Sale>, returns the list of the file that was opened
+        private void main(string temp) 
+        {
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(temp).ToList();
+            foreach (string line in lines)
+            {
+                List<string> item = line.Split(',').ToList();
+                Sale s = new Sale();
+                s.Textbook = item[0];
+                s.Subject = item[1];
+                s.Seller = item[2];
+                s.Purchaser = item[3];
+                try
+                {
+                    s.PurchasePrice = float.Parse(item[4]);
+                    s.SalePrice = item[5];
+                    s.Rating = item[6];
+                }
+                catch
+                {
+                    Console.WriteLine("Parse Fail");       
+                }
+                sales.Add(s);
+
+            }
+            //add to the binding source outside of the fucntion as to not overload the program
+            bs.DataSource = sales;
+            bs.ResetBindings(false);
+        }
+
+
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+
+        //Function: sort the list that is given
+        //Input: List<sales> tempList, a list under a class
+        //OutPut: The sorted version of the inputed list
+        private void selectionSort(List<Sale> tempList)
+        {
+            int min;
+            string temp;
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < tempList.Count; j++)
+                {
+                    if (int.TryParse(tempList[j].Rating, out int rating))
+                    {
+                        if (int.TryParse(tempList[min].Rating, out int ratingMin))
+                        {
+                            if (rating < ratingMin)
+                            {
+                                min = j;
+                            }
+                        }
+                        else
+                        {
+                            min = j;
+                        }
+                    }
+                }
+                temp = tempList[min].Rating;
+                tempList[min].Rating = tempList[i].Rating;
+                tempList[i].Rating = temp;
+            }
+
+            //if the user wants to sort asc then reverse the list
+            if (ASC)
+            {
+                tempList.Reverse();
+            }
+        }
+
+        //Check what the user wants to search by
+        private void cmbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filter = cmbFilter.Text;
+            if (filter == "Rating")
+            { 
+                selectionSort(sales); 
+            }
+            dataGridView1.DataSource = bs;
+            bs.ResetBindings(false);
+        }
+
+        //check to see if ASC or DESC for rating
+        private void cmbAsc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbAsc.Text == "ASC")
+            {
+                ASC = true;
+                selectionSort(sales);
+            }
+            if(cmbAsc.Text == "DESC")
+            {
+                ASC = false;
+                selectionSort(sales);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            List<Sale> s = Search(txtSearch.Text, filter);
+            bs.DataSource = s;
+            dataGridView1.DataSource = s;
+            bs.ResetBindings(false);
+        }
+
+        //Function: remove parts of a list to sort it
+        //Input: string target, string filter, Use to choose what is getting selected
+        //Output: returns new list.
+        private List<Sale> Search(string target, string filter)
+        {
+            List<Sale> results = new List<Sale>();
+            foreach (Sale s in sales)
+            {
+                if (filter == "Rating")
+                {
+                    if (s.Rating.ToLower() == target.ToLower()) results.Add(s);
+                }
+                if (filter == "Textbook")
+                {
+                    if (s.Textbook.ToLower().Contains(target.ToLower())) { results.Add(s); }
+                }
+
+                if (filter == "Subject")
+                {
+                    if (s.Subject.ToLower().Contains(target.ToLower())) { results.Add(s); }
+                }
+
+            }
+            return results;
+        }
+    }
+}
